63b8d2b1c008532e9097c84e00c906dcbd1cb96d
Sujay Jeppu <sujayrj@gmail.com>
2021-09-18 09:46:25 +0530

    RequestParam demo

--- src/main/java/com/jeppu/controller/ProductController.java
+++ src/main/java/com/jeppu/controller/ProductController.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping(value={"/product"})
+@RequestMapping(value = {"/product"})
 public class ProductController {
 
     private final ProductService productService;
@@ -19,10 +20,21 @@ public class ProductController {
         this.productService = productService;
     }
 
-
     @GetMapping(value = "/all")
-    public String getAllProducts(Model model){
+    public String getAllProducts(Model model) {
         model.addAttribute("products", productService.getAllProducts());
         return "all-products";
+    }
+
+    //http://localhost:8080/spring_mvc_war_exploded/product/listProducts?categoryId=1&categoryName=Laptop
+    //---When Categoryname is ommitted in the URL
+    //http://localhost:8080/spring_mvc_war_exploded/product/listProducts?categoryId=1
+    @RequestMapping(value = "/listProducts")
+    public String getProductByCategory(@RequestParam String categoryId, @RequestParam(value = "categoryName", required = false, defaultValue = "IPhone") String name, Model model) {
+        System.out.println("Category ID : " + categoryId);
+        System.out.println("Category Name : " + name);
+        model.addAttribute("categoryId", categoryId);
+        model.addAttribute("categoryName", name);
+        return "list-products";
     }
 }
---
+++ src/main/webapp/WEB-INF/view/list-products.jsp
@@ -0,0 +1,18 @@
+<%--
+Created by IntelliJ IDEA.
+User: sujay
+Date: 16-09-2021
+Time: 02:51 PM
+To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8"%>
+<html>
+<head>
+    <title>List Prodcuts</title>
+</head>
+    <body>
+        <h1>List Products</h1>
+        <p>Product Category Id : ${categoryId}</p>
+        <p>Product Category Id : ${categoryName}</p>
+    </body>
+</html>
